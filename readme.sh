ae8f0aea37edbe26dbabd4b1183817fe6f254343
oudream <oudream@126.com>
2019-11-12 20:00:56 +0800

    d

--- readme.sh
+++ readme.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+### cmake : gtest_add_tests
+# https://cmake.org/cmake/help/v3.10/module/GoogleTest.html
+
+### example
+# http://senlinzhan.github.io/2017/10/08/gtest/
+
 cd /opt/ddd/ops/jenkins/hello-jenkinsfile
 cmake . -DCMAKE_BUILD_TYPE=Debug --build "/opt/ddd/ops/jenkins/hello-jenkinsfile" -B"/opt/ddd/ops/jenkins/hello-jenkinsfile/cmake-gcc"
 cd cmake-gcc && make
