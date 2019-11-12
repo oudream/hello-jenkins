3f649381e5b600f655998b6378a75291c3e2d71d
oudream <oudream@126.com>
2019-11-12 19:56:17 +0800

    up

--- readme.sh
+++ readme.sh
@@ -3,5 +3,5 @@
 cd /opt/ddd/ops/jenkins/hello-jenkinsfile
 cmake . -DCMAKE_BUILD_TYPE=Debug --build "/opt/ddd/ops/jenkins/hello-jenkinsfile" -B"/opt/ddd/ops/jenkins/hello-jenkinsfile/cmake-gcc"
 cd cmake-gcc && make
-export GTEST_OUTPUT="xml:/home/unittest-report"
+export GTEST_OUTPUT="xml:/opt/ddd/tmp/unittest-report-cpp"
 make test
