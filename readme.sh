b31bd1f9feb58503b42952a2c398e432fb34dd17
oudream <oudream@126.com>
2019-11-12 20:20:10 +0800

    Update readme.sh

--- readme.sh
+++ readme.sh
@@ -15,3 +15,9 @@ cmake . -DCMAKE_BUILD_TYPE=Debug --build "/opt/ddd/ops/jenkins/hello-jenkinsfile
 cd cmake-gcc && make
 export GTEST_OUTPUT="xml:/opt/ddd/tmp/unittest-report-cpp"
 make test
+
+cd /opt/ddd/ops/jenkins/hello-jenkinsfile
+cmake . -DCMAKE_BUILD_TYPE=Debug --build "/opt/ddd/ops/jenkins/hello-jenkinsfile" -B"/opt/ddd/ops/jenkins/hello-jenkinsfile/cmake-gcc"
+cd cmake-gcc && make
+export GTEST_OUTPUT="xml:/opt/ddd/tmp/unittest-report-cpp"
+make test
