554554b54a9e96b3a89168dc0ac459780edaf7e7
oudream <oudream@126.com>
2019-11-13 17:14:45 +0800

    up

--- readme.sh
+++ readme.sh
@@ -16,8 +16,33 @@ cd cmake-gcc && make
 export GTEST_OUTPUT="xml:/opt/ddd/tmp/unittest-report-cpp"
 make test
 
-cd /opt/ddd/ops/jenkins/hello-jenkinsfile
+
+docker run \
+  -u root \
+  --rm \
+  -d \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  jenkinsci/blueocean
+
+
+wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war && \
+java -jar jenkins.war --httpPort=2280
+
+java -jar jenkins.war --httpPort=2280
+
+yepbvuanunoybjfi
+xwliqktnpykkbiea
+
+
+cd $WORKSPACE
+export GTEST_OUTPUT="xml:${WORKSPACE}/unittest-report"
 cmake . -DCMAKE_BUILD_TYPE=Debug --build "/opt/ddd/ops/jenkins/hello-jenkinsfile" -B"/opt/ddd/ops/jenkins/hello-jenkinsfile/cmake-gcc"
-cd cmake-gcc && make
-export GTEST_OUTPUT="xml:/opt/ddd/tmp/unittest-report-cpp"
+
+
+export GTEST_OUTPUT="xml:${WORKSPACE}/unittest-report"
+cd ${WORKSPACE}/build
+make
 make test
